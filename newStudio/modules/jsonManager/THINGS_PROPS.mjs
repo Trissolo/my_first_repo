dc606d14d01c9c221e773af2cc892575761910b9
Trissolo <00tristano00@gmail.com>
2023-02-26 20:38:34 +0100

    Rename 'base.mjs' -> 'THINGS_PROPS.mjs'

---
+++ newStudio/modules/jsonManager/THINGS_PROPS.mjs
@@ -0,0 +1,44 @@
+/*
+coords,
+name,
+frame,
+frameSuffix,
+frameStem
+depth,
+rect,
+noInput,
+hoverName,
+skipCond,
+*/
+
+// *Thing*:
+
+// x,
+// y,
+// frame,
+
+// frameSuffix,
+// skipCond,
+// hoverName,
+// noInput,
+// animation,
+// rid
+
+// *TriggerArea*:
+
+// x,
+// y,
+// width,
+// height,
+// skipCond,
+// hoverName,
+// actors,
+// rid
+
+export default {
+    FRAME_SUFFIX: "frameSuffix",
+    SKIP_CONDITION: "skipCond",
+    HOVER_NAME: "hoverName",
+    NO_INPUT: "noInput",
+    ANIMATION_NAME: "animation"
+}

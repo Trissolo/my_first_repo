0f3b7bfdc5324bf8bbd817af4d97fbd916b9ec6f
Trissolo <00tristano00@gmail.com>
2023-03-03 15:42:56 +0100

    Update DepthCategories

---
+++ newStudio/modules/jsonManager/DepthCategories.mjs
@@ -0,0 +1,10 @@
+export default {
+    "bg": -10, // BACKGROUND
+    "rf": 0, // RIDICULOUSLYFARAWAY
+    "ta": 1, // TRIGGERZONE
+    "co": 2, // COVERED
+    "ab": 3, // ALWAYSBACK
+    "ds": 4, // DEEPSORTED
+    "fg": 800 // FOREGROUND
+    }
+    

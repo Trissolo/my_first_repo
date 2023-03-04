196ddc3214fd9a611064bb09aae3aab59e485753
Trissolo <00tristano00@gmail.com>
2023-03-04 08:27:34 +0100

    Group some modules

---
+++ newStudio/modules/widget/classes/baseClassesWrapper.mjs
@@ -0,0 +1,9 @@
+import BaseWidget from "./baseWidget.mjs";
+import PseudoButton from "./pseudoButton.mjs";
+import TextField from "./textField.mjs";
+
+export default {
+    BaseWidget,
+    PseudoButton,
+    TextField
+}
---
+++ newStudio/modules/widget/classes/labels.mjs
@@ -0,0 +1,9 @@
+const NOBREAK_SPACE= " ";
+
+export default {
+    NOBREAK_SPACE,
+    labelNextJson: `${NOBREAK_SPACE}Next Room ▶${NOBREAK_SPACE}`,
+    labelPrevJson: `${NOBREAK_SPACE}◀ Prev Room${NOBREAK_SPACE}`,
+    labelNextThing: `${NOBREAK_SPACE}Next Thing ▶${NOBREAK_SPACE}`,
+    labelPrevThing: `${NOBREAK_SPACE}◀ Prev Thing${NOBREAK_SPACE}`
+}

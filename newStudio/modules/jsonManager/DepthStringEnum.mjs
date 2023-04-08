4d6dd0eb822d3804f769d57c874d80df212ba23a
Trissolo <00tristano00@gmail.com>
2023-04-08 22:51:56 +0200

    Add DepthStringEnum

---
+++ newStudio/modules/jsonManager/DepthStringEnum.mjs
@@ -0,0 +1,10 @@
+export default {
+    "BACKGROUND": "bg",
+    "RIDICULOUSLYFARAWAY": "rf",
+    "TRIGGERZONE": "ta",
+    "COVERED": "co",
+    "ALWAYSBACK": "ab",
+    "DEEPSORTED": "ds",
+    "FOREGROUND": "fg"
+    }
+    

56fa9f709308e6b4d02fc4489cd440969b139a73
Trissolo <00tristano00@gmail.com>
2023-04-16 01:00:54 +0200

    Ref

---
+++ src/plugins/VarsManager/OldWayVarManager.mjs
@@ -0,0 +1,170 @@
+const BITS_PER_TYPED_ARRAY_ELEMENT = 32; //from "./BITS_PER_TYPED_ARRAY_ELEMENT.mjs";
+
+const {ToXY, Clamp, MaxAdd, MinSub} = Phaser.Math;
+
+export default class AllVarsManager
+{
+    static vers = "orco 1";
+
+    static varContainers = new Map();
+
+    // sort of constructor
+    // kind/key(containerIdx)|     varSize     |varsPerElement|bitmask
+    // ----------------------|-----------------|--------------|-------
+    //  0                    | 1 bit  (BOOL)   |      32      |   1   
+    //  1                    | 2 bits (CRUMBLE)|      16      |   3   
+    //  2                    | 4 bits (NIBBLE) |       8      |   15  
+    //  3                    | 8 bits (BYTE)   |       4      |   255 
+
+    static initialize()
+    {      
+
+        for (let kind = 0; kind < 4; kind++)
+        {
+
+            this.varContainers.set(this.varContainers.size, this.createByKind(kind));
+
+        }
+
+        return this;
+        
+    }  // end Initialize
+
+    /*
+
+    * @param {number} kind - an integer between 0 and 3
+    * @param {number|number[]} arrayLength - The wanted 'length' of the Typed Array
+    * 
+    * @return an Object in the form:
+    * {
+    *     typedArray: Uint32Array,
+    *     varSize: number,
+    *     varsPerElement: number,
+    *     bitmask: number,
+    *     isBool: boolean
+    * }
+    * 
+    */
+
+    static createByKind(kind, arrayLength = 2)
+    {
+        const varSize = 1 << kind;
+
+        // amount of variables in each Typed Array element
+        const varsPerElement = BITS_PER_TYPED_ARRAY_ELEMENT / varSize;
+
+        // bitmask to extract/work on the variable
+        // It also indicates the maximum possible value of the variable
+        const bitmask = (1 << varSize) - 1;
+
+        if (Array.isArray(arrayLength))
+        {
+            arrayLength = Math.ceil(arrayLength.length * varSize / BITS_PER_TYPED_ARRAY_ELEMENT);
+        }
+
+        const typedArray = new Uint32Array(arrayLength);
+
+        const coords = new Phaser.Math.Vector2();
+
+        return {varSize, varsPerElement, bitmask, typedArray, coords, isBool: varSize === 1};
+    }
+
+    // Calculate coords:
+
+    /*
+    * @param {number} container - Pass 'null' if all other args are passed in
+    * @param {number} varIdx -
+    * 
+    * @param {varsPerElement} -
+    * @param {typedArray} - 
+    * @param {coords} - 
+    */
+
+    // static prepareCoords(container, varIdx)
+    static prepareCoords(container, varIdx, varsPerElement = container.varsPerElement, typedArray = container.typedArray, coords = container.coords)
+    {
+        ToXY(varIdx, varsPerElement, typedArray.length, coords);
+
+        return container;
+    }
+
+    // User methods:
+
+    static setVar(containerIdx, varIdx, newValue)
+    {
+        // const { typedArray, varSize, varsPerElement, bitmask, coords: {x, y} } = this.prepareCoords(this.varContainers.get(containerIdx), varIdx);
+
+        const container = this.prepareCoords(this.varContainers.get(containerIdx), varIdx);
+
+        const {x, y} = container.coords;
+
+        return this.assignValueAt(newValue, x, y, container);
+    }
+
+    // end User methods
+
+
+    // Internal methods:
+
+    static assignValueAt(newValue, x, y, {typedArray, varSize, bitmask})
+    {
+        // just in case...
+        newValue = Clamp(newValue, 0, bitmask);
+
+        const currentSize = x * varSize;
+
+        //clear first:
+        typedArray[y] &= ~(bitmask << currentSize); 
+
+        if (newValue === 0)
+        {
+            return newValue;
+        }
+
+        return typedArray[y] |= (newValue << currentSize);
+    }
+
+    static setBoolOnAt(x, y, typedArray)
+    {
+        typedArray[y] |= 1 << x;        
+    }
+
+    static clearBoolAt(x, y, typedArray)
+    {
+        typedArray[y] &= ~(1 << x);      
+    }
+
+    static toggleBoolAt(x, y, typedArray)
+    {
+        typedArray[y] ^= (1 << x);
+    }
+
+    // End Internal methods:
+
+
+
+    // debug a scazzo
+    static debugTAry(containerIdx = 0, max) // , elemIdx = 0)
+    {
+        const container = this.varContainers.get(containerIdx);
+
+        max = max || container.varsPerElement * container.typedArray.length;
+
+        // console.log("max Var idx", max);
+
+        for (let i = 0; i < max ; i++)
+        {
+
+            this.prepareCoords(container, i);
+
+            const res = (container.typedArray[container.coords.y] >>> container.coords.x * container.varSize) & container.bitmask;
+
+            if ((i % container.varsPerElement) === 0)
+            {
+                console.log(`*** varsContainer.get(${containerIdx})[${i / container.varsPerElement}] ***`);
+            }
+            console.log(`${i}) (---) ${res} ${res.toString(2).padStart(container.varSize, "0")}`)
+            
+        }
+    }
+}

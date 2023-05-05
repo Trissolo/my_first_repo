7a2b1c2a10e80e8fa727f10da9f3ec655b71ef9e
Trissolo <00tristano00@gmail.com>
2023-05-05 07:11:04 +0200

    Add test Array

--- newStudio/modules/placeholders/InGameArrays.mjs
+++ newStudio/modules/placeholders/InGameArrays.mjs
@@ -3,10 +3,13 @@ import arrayIds from "./arrayIds.mjs";
 import Conditions from "./Conditions.mjs";
 import OnHoverNames from "./OnHoverNames.mjs";
 
+import TestCrumbleNames from "../widget/studiowidgets/skipCondition/TestCrumbleNames.mjs";
+
 const originalArrays = new Map([
     [arrayIds.Bool, Conditions],
+    [arrayIds.Crumble, TestCrumbleNames],
     [arrayIds.HoverNames, OnHoverNames]
-])
+]);
 
 export default {
     arrayIds,
